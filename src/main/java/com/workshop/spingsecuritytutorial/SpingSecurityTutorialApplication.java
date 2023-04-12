29c3905eb1d68a0869e437cdc6c2c05a0f5f7d28
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-12 15:17:18 +0400

    Initial commit

---
+++ src/main/java/com/workshop/spingsecuritytutorial/SpingSecurityTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.spingsecuritytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingSecurityTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingSecurityTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workshop/spingsecuritytutorial/SpingSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.spingsecuritytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingSecurityTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
