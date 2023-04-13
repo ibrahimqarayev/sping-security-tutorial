091c8bdf286c625166a23211afb41228364a5faa
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-13 14:40:43 +0400

    Added AuthenticationManager bean

--- src/main/java/com/workshop/spingsecuritytutorial/config/SecurityConfig.java
+++ src/main/java/com/workshop/spingsecuritytutorial/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.workshop.spingsecuritytutorial.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -65,6 +67,12 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
